de08fb1f1b2ddea0f6aae74ad4750d590c9737e8
Aerydis <aerydis700@gmail.com>
2026-07-01 17:39:32 +0900

    jean post
    fixed formatting

--- visual/script.js
+++ visual/script.js
@@ -28,12 +28,14 @@ const posts = [
 //         url: 'photos/photo.webp',
 //         content: formatCaption(`photo caption example`)
 //     }, 
-    type: '시험에서 만점 받길 바랄게!',
-        date: 'June 29, 2026',
-        time: '8:37 KST',
-        content: "Hey, I know your exams are coming up in July. Good luck with studying—you’ve got this. Don’t forget to rest too. I’m rooting for you.(by jean)"
-
 //     paste after here!
+
+    {
+        type: 'text',
+        date: 'June 29, 2026',
+        time: '6:37 VST',
+        content: "Hey, I know your exams are coming up in July. Good luck with studying—you’ve got this. Don’t forget to rest too. I’m rooting for you.(by jean)"
+    },
     {
         type: 'text',
         date: 'June 29, 2026',
